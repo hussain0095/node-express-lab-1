1d1423c4baaf0d8cd5838a25c60dd78e3db5a94f
hussain0095 <hussain.hameed95@outlook.com>
2025-12-23 22:28:08 +0300

    Intro to Express Lab

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "about.html"));
+});
+
+app.get("/test", (req, res) => {
+  res.send("<p>Success!</p>");
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port 3000");
+});
