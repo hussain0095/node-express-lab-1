659476541837b62a145423f00db6d8c84bb96fd1
hussain0095 <hussain.hameed95@outlook.com>
2025-12-23 23:14:30 +0300

    Add public folder and CSS styling

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
+app.use(express.static("public"));
+
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
